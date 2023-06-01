c70cfc13b39635e28af54b4db24e10921fe2fa97
anup-an <friek_an@yahoo.com>
2023-06-01 14:23:49 +0300

    Add responsiveness

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -6,7 +6,7 @@ const NavBar = (props: any) => {
   return (
     <div className="NavBar">
       <Link to="/" className={`${focus === 'about' ? 'highlight-link' : ''}`}>About</Link>
-      <Link to="/technical-skills" className={`${focus === 'technical-skills' ? 'highlight-link' : ''}`}>Technical skills</Link>
+      <Link to="/technical-skills" className={`${focus === 'technical-skills' ? 'highlight-link' : ''}`}>Skills</Link>
       <Link to="/projects" className={`${focus === 'projects' ? 'highlight-link' : ''}`}>Projects</Link>
       <Link to="/experience" className={`${focus === 'experience' ? 'highlight-link' : ''}`}>Experience</Link>
     </div>
--- src/views/SkillsPage/index.tsx
+++ src/views/SkillsPage/index.tsx
@@ -1,16 +1,15 @@
 import NavBar from "../../components/NavBar";
 import Skills from "../../components/Skills";
-import "./SkillsPage.scss"
+import "./SkillsPage.scss";
 
 const SkillsPage = () => {
-    return (
-        <div className="about-page">
-            <NavBar focus="technical-skills"/>
-            <div className="about-page__content">
-
-            <Skills />
-            </div>
+  return (
+    <div className="skills-page">
+      <NavBar focus="technical-skills" />
+      <div className="skills-page__content">
+        <Skills />
       </div>
+    </div>
   );
 };
 
